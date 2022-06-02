d17b5d5e09f8c6bf51b55bc192940491f29e75e8
Cayde7 <43235246+Cayde7@users.noreply.github.com>
2022-06-02 11:14:45 +0200

    make it better

---
+++ src/hello.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/prefer-default-export */
+
+/**
+ *
+ * @function helloJS
+ * @returns <String> - returns the string 'Hello JavaScript'
+ */
+ function helloJS() {
+    return 'Hello JavaScript';
+}
+
+export {
+  helloJS,
+};
